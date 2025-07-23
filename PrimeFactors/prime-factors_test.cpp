7441c55ce04b76b2c7ecb5a16aa8b8ac1d40db6f
sh1.lee <sh1.lee@samsung.com>
2025-07-23 11:55:05 +0900

    [refactoring] 64

--- PrimeFactors/prime-factors_test.cpp
+++ PrimeFactors/prime-factors_test.cpp
@@ -35,3 +35,9 @@ TEST_F(PrimeFixture, Of6) {
     expected = { 2, 3 };
     EXPECT_EQ(expected, prime_factor.of(6));
 }
+
+TEST_F(PrimeFixture, Of9) {
+    PrimeFactors prime_factor;
+    expected = { 3, 3 };
+    EXPECT_EQ(expected, prime_factor.of(9));
+}
